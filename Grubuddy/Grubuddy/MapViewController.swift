2201f6a79ac027dd6c0a470d6c3b864c03015992
Junyan Kang <kelvinhongkjy@gmail.com>
2016-11-30 17:31:44 -0800

    Fetch meetings and display annotations in map view

--- Grubuddy/Grubuddy/MapViewController.swift
+++ Grubuddy/Grubuddy/MapViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import CoreData
+import MapKit
 
 class MapViewController: UIViewController {
 
     
     @IBOutlet weak var my_title: UILabel!
     @IBOutlet weak var members: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        loadAnnotaions()
     }
 
-  
+    func loadAnnotaions () {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let meetingRequest: NSFetchRequest<Meeting> = Meeting.fetchRequest()
+        do {
+            let fetchedMeetings = try context.fetch(meetingRequest)
+            print(fetchedMeetings)
+            mapView.addAnnotations(fetchedMeetings)
+        } catch {
+            fatalError("Failed to fetch meetings: \(error)")
+        }
+    }
+
+    func updateOverlay(meeting: Meeting) {
+        
+        // TODO: update labels
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
