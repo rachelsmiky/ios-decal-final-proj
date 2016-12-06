7420b7da58f92e181d127830cb0f4ed0f45ab3b7
Junyan Kang <kelvinhongkjy@gmail.com>
2016-12-05 18:24:29 -0800

    Add login handler skeleton

--- Grubuddy/Grubuddy/LoginViewController.swift
+++ Grubuddy/Grubuddy/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class LoginViewController: UIViewController {
 
@@ -19,23 +20,18 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func login(withUsername username: String, password: String) {
+        if username.characters.count > 0 && password.characters.count > 0 {
+            
+            // TODO: 1. create a NSFetchRequest
+            //       2. Add a predicate to the fetch request
+            //       3. if no matched user is found, create a new one, using NSEntityDescription.insertNewObject
+            //       4. store user in AppDelegate
+            
+            dismiss(animated: true, completion: nil)
+        }
     }
-    */
 
 }
